aab2e4189fcf855016273a4674b92a9c93e08df9
Andreia Souza <fsouza.andreia@gmail.com>
2022-10-25 04:49:59 +0100

    Initial commit :tada:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/* style */
+import "./global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#090312", minHeight: "100vh" }}>
+      <div style={{ color: "#eaeaea" }} className="App">
+        HELLO PORTFOLIO
+      </div>
     </div>
   );
 }
