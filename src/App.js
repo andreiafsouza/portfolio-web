7fed19b82a072059ef62554a051704d8701dae3d
Andreia Souza <fsouza.andreia@gmail.com>
2022-10-26 03:45:45 +0100

    style: adding palletes and dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 /* style */
 import "./global.css";
+import { colors } from "./stylesheets/abstracts/palettes";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#090312", minHeight: "100vh" }}>
-      <div style={{ color: "#eaeaea" }} className="App">
+    <div style={{ minHeight: "100vh", backgroundColor: colors.primary[900] }}>
+      <div style={{ color: colors.neutral[100] }} className="App">
         HELLO PORTFOLIO
       </div>
     </div>
---
+++ src/stylesheets/themes/themes.js
@@ -0,0 +1,8 @@
+import { darkPalette } from "../abstracts/palettes";
+
+export const darkTheme = {
+  primary: {
+    text: darkPalette.neutral[100],
+    background: darkPalette.primary[900],
+  },
+};
